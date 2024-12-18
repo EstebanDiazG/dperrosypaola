029b75a2e6c725e1f76306c94e1d19dc85a422a4
EstebanDiazG <esteban.diaz@sentrica.cl>
2024-12-18 00:47:17 -0300

    footer layout plus body

--- front/components/body/body.jsx
+++ front/components/body/body.jsx
@@ -1,44 +1,124 @@
 import React from "react";
+import Image from "next/image";
 
 const Body = () => {
   return (
     <main className=" text-gray-800">
-      {/* Sección de Introducción */}
-      <section className=" text-black py-[100px]">
-        <div className="container mx-auto px-4 text-center">
-          <h1 className="text-4xl font-bold mb-4">
-            Bienvenidos a Mi Sitio Web
-          </h1>
-          <p className="text-lg">
-            Aquí encontrarás los mejores servicios y productos adaptados a tus
-            necesidades.
-          </p>
+      {/* Botón flotante de WhatsApp */}
+      <a
+        href="https://wa.me/+56945188434"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="whatsapp-float"
+        aria-label="Contactar por WhatsApp"
+      >
+        <img src="/whatsapp.svg" alt="WhatsApp" width={50} height={50} />
+      </a>
+      {/* Sección de Quiénes Somos */}
+      <section id="quienes-somos" className="text-black py-[100px]">
+        <div className="container mx-auto px-4 text-center md:text-left flex flex-col md:flex-row items-center justify-between">
+          <div className="md:w-1/2 mb-8 md:mb-0">
+            <h1 className="text-4xl font-bold mb-4">Quiénes Somos</h1>
+            <p className="text-lg mb-6">
+              En <strong>De Perros y Paola</strong>, somos un equipo apasionado
+              por el bienestar animal. Nos dedicamos a brindar servicios de
+              educación canina, socialización y modificación de conducta,
+              siempre con el objetivo de fortalecer la relación entre tú y tu
+              compañero peludo.
+            </p>
+            <p className="text-lg mb-6">
+              Con años de experiencia, Paola Poblete, nuestra entrenadora
+              profesional, utiliza métodos basados en el refuerzo positivo para
+              enseñar a los perros de manera amable, respetuosa y efectiva.
+              Creemos en la importancia de una comunicación clara y empática
+              entre el perro y su dueño para lograr una convivencia armoniosa.
+            </p>
+            <p className="text-lg">
+              Nos enorgullece ofrecer un enfoque personalizado, adaptado a las
+              necesidades individuales de cada perro, porque sabemos que cada
+              uno es único. Ya sea que estés buscando mejorar el comportamiento
+              de tu mascota o fortalecer su socialización, estamos aquí para
+              ayudarte.
+            </p>
+          </div>
+          <div className="md:w-1/2 md:px-28 transform transition-transform duration-300 hover:scale-105 ">
+            <Image
+              src="/quienes-somos-1.png"
+              alt="Quiénes Somos"
+              width={500}
+              height={400}
+              className="rounded-lg shadow-lg object-cover"
+            />
+          </div>
         </div>
       </section>
 
       {/* Sección de Servicios */}
-      <section className="py-12">
+      <section id="servicios" className="py-12">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-6">
+          <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">
             Nuestros Servicios
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <div className="bg-white p-6 shadow-lg rounded-lg text-center">
-              <h3 className="text-xl font-bold mb-2">Servicio 1</h3>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+            {/* Servicio 1 */}
+            <div className="bg-white p-6 shadow-lg rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+              <h3 className="text-xl font-semibold text-[#f58f9a] mb-4">
+                🐾 Comunicación Efectiva con tu Perro
+              </h3>
               <p className="text-gray-600">
-                Descripción breve del servicio 1. Adaptado a tus necesidades.
+                La comunicación efectiva es clave para establecer una relación
+                sólida y armónica con tu perro. A través de técnicas de
+                adiestramiento basadas en el refuerzo positivo, aprenderás cómo
+                entender mejor las necesidades de tu mascota y cómo enseñarle
+                comportamientos adecuados. Este servicio te ayudará a fortalecer
+                el vínculo con tu compañero canino, promoviendo una convivencia
+                más tranquila y satisfactoria. Ya sea que tengas un cachorro o
+                un perro adulto, nuestro enfoque personalizado se adapta a sus
+                necesidades y características particulares, asegurando una
+                comunicación clara y efectiva.
               </p>
             </div>
-            <div className="bg-white p-6 shadow-lg rounded-lg text-center">
-              <h3 className="text-xl font-bold mb-2">Servicio 2</h3>
+
+            {/* Servicio 2 */}
+            <div className="bg-white p-6 shadow-lg rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+              <h3 className="text-xl font-semibold text-[#f58f9a] mb-4">
+                🐶 Socialización
+              </h3>
               <p className="text-gray-600">
-                Descripción breve del servicio 2. Calidad garantizada.
+                La socialización es fundamental para el desarrollo emocional y
+                psicológico de tu perro. Nuestro servicio está diseñado para
+                fomentar interacciones saludables, tanto con otros perros como
+                con personas, ayudando a tu mascota a sentirse más cómoda y
+                segura en diversos entornos sociales. A través de sesiones
+                guiadas, tu perro aprenderá a reaccionar adecuadamente ante
+                nuevas situaciones, reduciendo comportamientos temerosos o
+                agresivos. Además, las interacciones positivas con otros perros
+                fortalecen su capacidad para adaptarse a diferentes contextos,
+                mejorando su bienestar general y su capacidad de convivencia.
+                Este servicio es ideal para cachorros, perros adultos o aquellos
+                que necesiten mejorar su comportamiento social.
               </p>
             </div>
-            <div className="bg-white p-6 shadow-lg rounded-lg text-center">
-              <h3 className="text-xl font-bold mb-2">Servicio 3</h3>
+
+            {/* Servicio 3 */}
+            <div className="bg-white p-6 shadow-lg rounded-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl">
+              <h3 className="text-xl font-semibold text-[#f58f9a] mb-4">
+                🐕‍🦺 Modificación de Conducta
+              </h3>
               <p className="text-gray-600">
-                Descripción breve del servicio 3. Resultados excepcionales.
+                Si tu perro presenta comportamientos no deseados, como ladridos
+                excesivos, masticado destructivo, saltos sobre las personas o
+                actitudes agresivas, nuestro servicio de modificación de
+                conducta está diseñado para corregir esos problemas de manera
+                efectiva. Utilizamos técnicas de adiestramiento basadas en el
+                refuerzo positivo, enfocándonos en la eliminación de
+                comportamientos negativos y la enseñanza de alternativas más
+                adecuadas. Cada perro es único, por lo que nuestro enfoque se
+                adapta a las necesidades específicas de tu mascota, asegurando
+                resultados duraderos. A través de sesiones personalizadas,
+                aprenderás cómo gestionar y redirigir comportamientos
+                indeseados, mejorando la convivencia en el hogar y asegurando
+                que tu perro sea más equilibrado y obediente.
               </p>
             </div>
           </div>
--- front/components/footer/footer.jsx
+++ front/components/footer/footer.jsx
@@ -1,45 +1,53 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="bg-[#3c363b] text-white py-6">
       <div className="container mx-auto px-4">
         {/* Sección superior con enlaces y logo */}
-        <div className="flex flex-wrap justify-between items-center border-b border-gray-600 pb-4 mb-4">
+        <div className="flex flex-wrap justify-between  items-center border-b border-gray-600 pb-4 mb-4">
           <div>
-            <h2 className="text-xl font-bold">Mi Sitio Web</h2>
+            <Image
+              src="/footer.png"
+              alt="Footer Image"
+              width={150}
+              height={100}
+            />
           </div>
-          <ul className="flex space-x-4">
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-gray-400">
-                Contact
-              </a>
-            </li>
-          </ul>
-          <div className="flex space-x-4">
-            <a href="#" className="hover:text-gray-400">
-              <i className="fab fa-facebook"></i> {/* Puedes usar un icono */}
-            </a>
-            <a href="#" className="hover:text-gray-400">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="#" className="hover:text-gray-400">
-              <i className="fab fa-instagram"></i>
+          <div className="mt-8 grid grid-cols-1 md:grid-cols-3">
+            <div className="px-10">
+              <h3 className="text-lg font-semibold">Servicios</h3>
+              <p>🐾 Comunicación efectiva con tu perro</p>
+              <p>🐾 Socialización</p>
+              <p>🐾 Modificación de la conducta</p>
+            </div>
+
+            <div className="px-20">
+              <h3 className="text-lg font-semibold">Educadora</h3>
+              <p>Paola Poblete</p>
+              <p>Educadora Canina</p>
+            </div>
+            <div>
+              <h3 className="text-lg font-semibold">Atención</h3>
+              <p>Lunes a Viernes: 9:00 - 18:00</p>
+              <p>Sábado: 10:00 - 14:00</p>
+            </div>
+          </div>
+          <div className="flex space-x-4 mt-8 ">
+            <a
+              href="https://www.instagram.com/dperrosypaola/"
+              className="hover:text-gray-400 flex items-center"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/Instagram.svg"
+                alt="Instagram"
+                width={24}
+                height={24}
+              />
+              <span className="ml-2">dperrosypaola</span>
             </a>
           </div>
         </div>
@@ -47,7 +55,8 @@ const Footer = () => {
         {/* Sección inferior con copyright */}
         <div className="text-center">
           <p className="text-sm">
-            &copy; 2025 EstebanDG. Todos los derechos reservados.
+            &copy; 2025 Todos los derechos reservados deperrosypaola.cl |
+            Desarrollado por STBNDEV
           </p>
         </div>
       </div>
