4440c010396593335eee169b204499d4ecb89119
EstebanDiazG <esteban.diaz@sentrica.cl>
2024-12-19 01:16:32 -0300

    Elimina la configuración de exportación y optimización de imágenes en next.config.mjs

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  basePath: "/dperrosypaola",
-  images: {
-    unoptimized: true, // Desactiva la optimización de imágenes para exportación estática
-  },
 };
 
 export default nextConfig;
